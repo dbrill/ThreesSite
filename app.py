2da1770ef00bd549cb8a5218d0aba6994f6c0338
dbrill <dabr9981@colorado.edu>
2017-08-13 12:48:10 -0600

    console test

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from datetime import datetime
+from os import environ
+import sys
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+	print ('hello')
+	print environ.get('DATABASE_URL')
 	return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
