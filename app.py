a66c795dcf28dc562e237ac89ef3eff0763510e7
dbrill <dabr9981@colorado.edu>
2018-08-01 17:40:38 -0600

    web server simplification

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	print ('hello', file=sys.stderr)
-	print ('hello', file=sys.stdout)
-	print (environ.get('DATABASE_URL'))
 	return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
