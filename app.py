ac1f9657d42e548fc655b03887d4300dc0fc9465
dbrill <dabr9981@colorado.edu>
2020-04-15 18:47:50 -0600

    Add Hdox WhiteSheet Route

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
 	return render_template('index.html')
+
+@app.route('/hdox')
+def hdoxPage():
+    return render_template('hdox.html')
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
